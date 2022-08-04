4bad2942f25d8d3003f3dc2430d046a88ae3ccea
Nurislom-prime <shudguke@gmail.com>
2022-08-04 09:25:51 +0500

    qo'shish

---
+++ oxirgi raqamni top.py
@@ -0,0 +1,3 @@
+a = int (input())
+S = a%10
+print (S)
---
+++ python_1.py
@@ -0,0 +1,32 @@
+a = 8
+b = int (input() )
+d = 3
+c = int (input() )
+x = -2
+x1 = 4
+R = 2
+R1 = 8
+R2 = 3
+p=3.14
+P = 4*a
+S = a*a
+S1 = a*b
+P1 = 2*(a+b)
+L = 3.14*d
+V = a*a*a
+S2 = 6*a*a
+V1 = a*b*c
+S3 = 2*(a*b+b*c+c*a)
+L1 = 2*3.14*R
+S4 = 3.14*R*R
+A = (a+b)/2
+Sa = p*R1
+Sb = p*R2
+S5 = Sa-Sb
+R3 = L/2/p
+S6 = p*R3*R3
+Masofa = x1-x
+Uzunlik = ((c-a)-(c-b))+(c-b)
+print(P,S,S1,P1,L,V,S2,V1,S3,L1,S4,A,S5,R3,S6,Masofa,Uzunlik)
+
+
